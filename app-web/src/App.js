2b7daeb3a4b8737b6b7af2ec3b47a95d8c61206b
sonle2003 <levietson2017@gmail.com>
2022-08-31 20:59:06 +0700

    new code

--- app-web/src/App.js
+++ app-web/src/App.js
@@ -1,12 +1,60 @@
 import "./App.css";
-import moto from "./merc.png";
+import image1 from "./merc.png";
+import React from "react";
+const user = {
+  id:10,
+  firstName: "Park",
+  lastName: "Chae-young",
+  nickName: "Rosé",
+  avatarUrl: "/images/Rose-Elle01.jpeg",
+  profile: "https://www.instagram.com/roses_are_rosie/",
+};
+const userLeft = [
+  {
+    id:1,
+    firstName: "Park1",
+    lastName: "Chae-young",
+    nickName: "Rosé",
+    avatarUrl: image1,
+    profile: "https://www.instagram.com/roses_are_rosie/",
+  },
+  {
+    id:2,
+    firstName: "Park2",
+    lastName: "Chae-young",
+    nickName: "Rosé",
+    avatarUrl: image1,
+    profile: "https://www.instagram.com/roses_are_rosie/",
+  },
 
-function sonle() {
-  return (
-    <div>
-      <p>hello</p>
+  {
+    id:3,
+    firstName: "Park3",
+    lastName: "Chae-young",
+    nickName: "Rosé",
+    avatarUrl: image1,
+    profile: "https://www.instagram.com/roses_are_rosie/",
+  },
+];
+const _print=(people)=>{
+  return(
+   <>
+    <div className="container">
+      <p className="id" style={{color: people.id % 2==0 ?"red":"green"}}>{people.id}</p>
+      <p className="first" >{people.firstName}</p>
+      <p className="last">{people.lastName}</p>
+      <p className="nick">{people.nickName}</p>
+      <img src={people.avatarUrl} width="200px" height="200px" className="img1"/>
     </div>
+   </>
+  );
+}
+function App() {
+  return (
+    <>
+      {userLeft.map((item)=><div key={item.id}>{_print(item)}</div>)}
+    </>
   );
 }
 
-export default sonle;
+export default App;
